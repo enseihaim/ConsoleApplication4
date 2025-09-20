68de5a1569abec1e0fb1ed2726a31e73e8fc2b6b
enseihaim <stroganov2009@gmail.com>
2025-09-20 14:42:51 +0300

    добавленно проверку

--- ConsoleApplication4.cpp
+++ ConsoleApplication4.cpp
@@ -3,16 +3,25 @@
 #include <Windows.h>
 using namespace std;
 
-class sqr {
+class sqr 
+{
+    
     static int count; 
 
 public:
     
     static double trHeron(double a, double b, double c)
     {
-        double s = (a + b + c) / 2.0;
-        count++;
-        return sqrt(s * (s - a) * (s - b) * (s - c));
+        if (a + b > c && a + c > b && b + c > a)
+        {
+            double s = (a + b + c) / 2.0;
+            count++;
+            return sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+        else
+        {
+            cout << "хибні данні, такого трикутника не існує" << endl;
+        }
     }
 
     static double trHeight(double base, double height)
@@ -33,12 +42,14 @@ public:
         return side * side;
     }
 
-    static double romb(double d1, double d2) {
+    static double romb(double d1, double d2)
+    {
         count++;
         return 0.5 * d1 * d2;
     }
 
-    static int Count() {
+    static int Count()
+    {
         return count;
     }
 };
@@ -50,7 +61,7 @@ int main() {
     SetConsoleOutputCP(1251); 
 
 
-    cout << "Площа трикутника (формула Герона): " << sqr::trHeron(3, 4, 5) << endl;
+    cout << "Площа трикутника (формула Герона): " << sqr::trHeron(1, 2, 5) << endl;
 
     cout << "Площа трикутника (через основу і висоту): " << sqr::trHeight(6, 4) << endl;
 
